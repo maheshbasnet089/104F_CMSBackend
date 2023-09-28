359de1fe6c2a85e6fcb66e0c285198848f4d8064
maheshbasnet089 <basnetmanish089@gmail.com>
2023-09-28 23:18:44 +0545

    add create api

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const { connectDatabase } = require("./database/database");
+const Blog = require("./model/blogModel");
+const express = require("express")
+const app = express();
 
-const app = require("express")();
 
-
+// nodejs lai form bata aako data parse gar vaneko ho 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
 
 // DATABASE CONNECTION FUNCTION
 connectDatabase()
@@ -16,6 +20,32 @@ app.get("/",(req,res)=>{
     })
 })
 
+
+// CREATE BLOG API  
+app.post("/createBlog",async (req,res)=>{
+   const title = req.body.title;
+   const subTitle = req.body.subTitle
+   const description = req.body.description
+
+//    Alternative (object destructuring)
+//    const {title,subTitle,description} = req.body
+
+    // Insert to database logic goes here 
+   await  Blog.create({
+        title : title  ,
+        subTitle : subTitle,
+        description : description
+    })
+
+    res.json({
+        status : 201,
+        message : "Blog created succesfully"
+    })
+    // Alternative 
+    // res.status(200).json({
+    //     message : "Blog created successfully"
+    // })
+})
 
 app.listen(2000,()=>{
     console.log("Nodejs has started at port 2000")
